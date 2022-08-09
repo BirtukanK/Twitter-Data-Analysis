57176b65ed17d04ead3b6ae5198e9c0df29ea673
BirtukanK <birtukankuma1113@gmail.com>
2022-08-09 21:31:32 +0300

    Bugs fixed

---
+++ clean_tweets_dataframe.py
@@ -0,0 +1,61 @@
+import pandas as pd
+class Clean_Tweets:
+    """
+    The PEP8 Standard AMAZING!!!
+    """
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+        print('Automation in Action...!!!')
+        
+    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        remove rows that has column names. This error originated from
+        the data collection stage.  
+        """
+        unwanted_rows = df[df['retweet_count'] == 'retweet_count'].index
+        df.drop(unwanted_rows , inplace=True)
+        df = df[df['polarity'] != 'polarity']
+        
+        return df
+    def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        drop duplicate rows
+        """
+        
+        df.drop_duplicates(inplace=True)
+        
+        return df
+    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        convert column to datetime
+        """
+        df['created_at'] = pd.to_datetime(df['created_at'])
+
+        df = df[df['created_at'] >= '2021-12-30']
+        return df
+    
+    def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        convert columns like polarity, subjectivity, retweet_count
+        favorite_count etc to numbers
+        """
+        df['polarity'] = pd.to_numeric(df['polarity'])
+        columns = ['polarity', 'friends_count',
+                   'retweet_count', 'followers_count',
+                   'subjuctivity', 'favorite_count']
+
+        for column in columns:
+            df[column] = pd.to_numeric(df[column])
+
+        df = df[df['created_at'] >= '2020-12-31']
+
+        return df
+    
+    def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        remove non english tweets from lang
+        """
+        
+        df = df[df['lang']=='en']
+        
+        return df
